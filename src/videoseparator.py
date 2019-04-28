f501d4ac1124d6280e4f1e39bc1bc5c2df2feeac
Fan1117 <macfan1995@gmail.com>
2019-04-27 20:01:25 -0400

    update

--- src/videoseparator.py
+++ src/videoseparator.py
@@ -6,15 +6,20 @@ Created on Fri Apr 26 11:57:00 2019
 """
 from ffmpy import FFmpeg
 import subprocess
+import os
 csv_out_path = '../../download/data/test_dict.csv'
 def process_and_save(dict_path):
     with open(dict_path, 'r') as f:
         reader = csv.reader(f)
         for row in reader:
             video_in_path = row[0]
-            video_out_path = '../../download/separated_data/' + '/video/' + row[2] + '-' + row[3] + '.mp4'
+            name = video_in_path.split('/')[-1]
+            name = name.strip('.mp4')
+            video_out_path = '../../download/separated_data/' + '/video/' + name + '.mp4'
+            #video_out_path = '../../download/separated_data/' + '/video/' + row[2] + '-' + row[3] + '.mp4'
             #video_out_path = row[3] + '.mp4'
-            wav_out_path = '../../download/separated_data/' + '/audio/' + row[2]  + '-' + row[3] + '.wav'
+            wav_out_path = '../../download/separated_data/' + '/audio/' + name + '.wav'
+            #wav_out_path = '../../download/separated_data/' + '/audio/' + row[2]  + '-' + row[3] + '.wav'
             try:
                 os.makedirs('../../download/separated_data/' + 'video')
             except FileExistsError:
