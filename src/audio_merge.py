fbcaab97d2ea2390c925be5a5aeccfa944a33700
Fan1117 <macfan1995@gmail.com>
2019-04-28 21:26:52 -0400

    audio_merge and spec_extraction

---
+++ src/audio_merge.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 28 13:02:37 2019
+
+@author: macfa
+"""
+import librosa
+import os
+import soundfile as sf
+
+
+audio_path = '../../download/separated_data/audio'
+mix_path = '../../download/mixture_data/audio'
+SR = 16000
+audio_num = 300
+def audio_merge(audio_path, mix_path, audio_num):
+    f = []
+    for (dirpath, dirnames, filenames) in os.walk(audio_path):
+        f.extend(filenames)
+        break
+    
+    f = f[:audio_num-1]
+    progress = 0
+
+    for i in range(len(f)):
+        path1 = f[i]
+        name1 = path1[:-4]
+        path2_count = i + 1
+        if path2_count == len(f):
+            break
+        while filenames[path2_count][:10] not in path1:
+            path2 = f[path2_count]
+            name2 = path2[:-4]
+            
+            ### load
+            signal1, _ = librosa.load(audio_path + '/' + path1, sr=SR)
+            signal2, _ = librosa.load(audio_path + '/' + path2, sr=SR)
+            ### 3.2s
+            signal1_slice = signal1[:3*SR]
+            signal2_slice = signal2[:3*SR]
+            ### normalize
+            signal1_n2 = librosa.util.normalize(signal1_slice, norm=2)
+            signal2_n2 = librosa.util.normalize(signal2_slice, norm=2)
+            ### merge
+            signal3 = signal1_n2 + signal2_n2
+            ### write mix_wav
+            try:
+                os.makedirs(mix_path)
+            except FileExistsError:
+                pass
+            name3 = name1 + '~' + name2 + '.wav'
+            dir3 = mix_path + '/' + name3
+            sf.write(dir3, signal3, samplerate=SR)
+            
+            progress += 1
+            if progress%100 == 1:
+                print("Progress: {0}/{1}".format(i,len(f)))
+#             ### feature extraction
+#             signal3_mel = librosa.feature.melspectrogram(y=signal3, sr=SR, n_mels=N_MEL)
+#             signal1_mel = librosa.feature.melspectrogram(y=signal1, sr=SR, n_mels=N_MEL)
+#             signal2_mel = librosa.feature.melspectrogram(y=signal2, sr=SR, n_mels=N_MEL)
+#             ### save h5py
+            path2_count += 1
+            if path2_count == len(f):
+                break
+
+audio_merge(audio_path, mix_path, audio_num)       
+
---
+++ src/spec_extraction.py
@@ -0,0 +1,190 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 28 13:59:34 2019
+
+@author: macfa
+"""
+import librosa
+import os
+import h5py
+import numpy as np
+from config import PARAS
+from IPython.display import Audio
+
+
+class MelConverter:
+
+    def __init__(self, sample_rate=PARAS.SR,
+                 n_fft=PARAS.N_FFT,
+                 #hop_length=PARAS.N_FFT // 4,
+                 hop_length=160,
+                 n_mel_freqs=PARAS.N_MEL,
+                 freq_min_hz=0, freq_max_hz=None):
+
+        self._SAMPLE_RATE = sample_rate
+        self._N_FFT = n_fft
+        self._HOP_LENGTH = hop_length
+        self._N_MEL_FREQS = n_mel_freqs
+        self._FREQ_MIN_HZ = freq_min_hz
+        self._FREQ_MAX_HZ = freq_max_hz
+
+        self._MEL_FILTER = librosa.filters.mel(
+            sr=self._SAMPLE_RATE,
+            n_fft=self._N_FFT,
+            n_mels=self._N_MEL_FREQS,
+            fmin=self._FREQ_MIN_HZ,
+            fmax=self._FREQ_MAX_HZ)
+
+    def signal_to_melspec(self, audio_signal, log=True, get_phase=False, transpose=False):
+        D = librosa.core.stft(audio_signal, n_fft=self._N_FFT, hop_length=self._HOP_LENGTH)
+        magnitude, phase = librosa.core.magphase(D)
+        mel_spectrogram = np.dot(self._MEL_FILTER, magnitude)
+        mel_spectrogram = mel_spectrogram ** 2
+
+        if log:
+            mel_spectrogram = librosa.power_to_db(mel_spectrogram)
+        if transpose:
+            mel_spectrogram = mel_spectrogram.T
+        if get_phase:
+            return mel_spectrogram, phase
+        else:
+            return mel_spectrogram
+
+    def melspec_to_audio(self, mel_spectrogram, log=True, phase=None, transpose=False, audio_out=True):
+        if transpose:
+            mel_spectrogram = mel_spectrogram.T
+        if log:
+            mel_spectrogram = librosa.db_to_power(mel_spectrogram)
+
+        mel_spectrogram = mel_spectrogram ** 0.5
+
+        magnitude = np.dot(np.linalg.pinv(self._MEL_FILTER), mel_spectrogram)
+
+        if phase is not None:
+            inverted_signal = librosa.istft(magnitude * phase, hop_length=self._HOP_LENGTH)
+        else:
+            inverted_signal = griffin_lim(magnitude, self._N_FFT, self._HOP_LENGTH, n_iterations=10)
+
+        if audio_out:
+            return Audio(inverted_signal, rate=self._SAMPLE_RATE)
+        else:
+            return inverted_signal
+
+
+def griffin_lim(magnitude, n_fft, hop_length, n_iterations):
+    """Iterative algorithm for phase retrival from a magnitude spectrogram."""
+    phase_angle = np.pi * np.random.rand(*magnitude.shape)
+    D = invert_magnitude_phase(magnitude, phase_angle)
+    signal = librosa.istft(D, hop_length=hop_length)
+
+    for i in range(n_iterations):
+        D = librosa.stft(signal, n_fft=n_fft, hop_length=hop_length)
+        _, phase = librosa.magphase(D)
+        phase_angle = np.angle(phase)
+
+        D = invert_magnitude_phase(magnitude, phase_angle)
+        signal = librosa.istft(D, hop_length=hop_length)
+
+    return signal
+
+
+def invert_magnitude_phase(magnitude, phase_angle):
+    phase = np.cos(phase_angle) + 1.j * np.sin(phase_angle)
+    return magnitude * phase
+#########################################################################
+SR = 16000
+separated_path = '../../download/separated_data/audio'
+mix_path = '../../download/mixture_data/audio'
+h5py_path = '../../dataset/no_video'
+
+
+def spec_extraction(mix_path, separated_path, h5py_path):
+    f = []
+    converter = MelConverter()
+    for (dirpath, dirnames, filenames) in os.walk(mix_path):
+        f.extend(filenames)
+        break
+
+    
+
+    
+    try:
+        os.makedirs(h5py_path)
+    except FileExistsError:
+        pass
+    
+    tr_name = h5py_path + '/' + 'tr_set.hdf5'
+    val_name = h5py_path + '/' + 'val_set.hdf5'
+    test_name = h5py_path + '/' + 'test_set.hdf5'
+    
+    tr_dataset = h5py.File(tr_name, 'a')
+    val_dataset = h5py.File(val_name, 'a')
+    test_dataset = h5py.File(test_name, 'a')
+    
+#    
+    
+    
+    
+    for i in range(len(f)):
+        file_mix = f[i]
+        file_1, file_2 = file_mix.split('~')
+        
+        path_mix = mix_path + '/' + file_mix
+        path_1 = separated_path + '/' + file_1 + '.wav'
+        path_2 = separated_path + '/' + file_2
+
+        
+        
+        
+        signal_mix, _ = librosa.load(path_mix, sr=SR)
+        signal_1, _ = librosa.load(path_1, sr=SR)
+        signal_2, _ = librosa.load(path_2, sr=SR)
+
+        ### convert
+        mel_mix = converter.signal_to_melspec(signal_mix[:3*SR], transpose=True)
+        mel_1 = converter.signal_to_melspec(signal_1[:3*SR], transpose=True)
+        mel_2 = converter.signal_to_melspec(signal_2[:3*SR], transpose=True)
+        
+        if i < 2000:
+            if i == 0:
+                tr_dataset.create_dataset('spec_mix', shape=(2000, 301, 150), dtype=np.float32)
+                tr_dataset.create_dataset('spec_1', shape=(2000, 301, 150), dtype=np.float32)
+                tr_dataset.create_dataset('spec_2', shape=(2000, 301, 150), dtype=np.float32)
+            tr_dataset['spec_mix'][i] = mel_mix
+            tr_dataset['spec_1'][i] = mel_1
+            tr_dataset['spec_2'][i] = mel_2
+            
+        elif i < 2500:
+            if i == 2000:
+                val_dataset.create_dataset('spec_mix', shape=(500, 301, 150), dtype=np.float32)
+                val_dataset.create_dataset('spec_1', shape=(500, 301, 150), dtype=np.float32)
+                val_dataset.create_dataset('spec_2', shape=(500, 301, 150), dtype=np.float32)
+            val_dataset['spec_mix'][i-2000] = mel_mix
+            val_dataset['spec_1'][i-2000] = mel_1
+            val_dataset['spec_2'][i-2000] = mel_2      
+            
+        elif i < 3000:
+            if i == 2500:
+                test_dataset.create_dataset('spec_mix', shape=(500, 301, 150), dtype=np.float32)
+                test_dataset.create_dataset('spec_1', shape=(500, 301, 150), dtype=np.float32)
+                test_dataset.create_dataset('spec_2', shape=(500, 301, 150), dtype=np.float32)
+            test_dataset['spec_mix'][i-2500] = mel_mix
+            test_dataset['spec_1'][i-2500] = mel_1
+            test_dataset['spec_2'][i-2500] = mel_2
+            
+        else:
+            break
+        
+        
+    tr_dataset.close()
+    val_dataset.close()
+    test_dataset.close()
+            
+
+
+spec_extraction(mix_path, separated_path, h5py_path)         
+            
+            
+        
+        
+        
