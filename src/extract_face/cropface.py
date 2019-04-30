3eae80b483bf2f0febb041cc18badeb5f4ea3363
Fan1117 <macfan1995@gmail.com>
2019-04-30 11:23:48 -0400

    update for frame

--- src/extract_face/cropface.py
+++ src/extract_face/cropface.py
@@ -1,6 +1,5 @@
 import os 
-import io
-import cv2
+
 from PIL import Image
 
 def crop(image_path, coords, saved_location):
@@ -14,56 +13,51 @@ def crop(image_path, coords, saved_location):
     cropped_image.save(saved_location)
     cropped_image.show()
 
-#paths = ['ymD5uLlLc0g_036.033000-040.900000','Swss72CHSWg_090.023267-097.297200',
-#        'Swss72CHSWg_090.023267-097.297200','AvWWVOgaMlk_090.000000-093.566667',
-#         'akwvpAiLFk0_144.680000-150.000000']
-
-import os
-frame_folder = '../../../download/separated_data/frame'
+#paths = ['019QoF6jwBU_150.633000-154.466000','6CUNIOtQ9L4_139.360000-144.440000',
+#         'sl08afxcx4_115.515400-119.986533','bLEddi92aFI_171.480000-177.400000']
+    
+faces_folder = '../../../new_download/separated_data/faces'
 
 paths = []
-for (dirpath, dirnames, filenames) in os.walk(frame_folder):
+for (dirpath, dirnames, filenames) in os.walk(faces_folder):
     paths.extend(dirnames)
     break
 
-
-
 for path in paths:
-    try:
-        os.makedirs('../../../download/separated_data/faces/'+path)
-    except FileExistsError:
-        pass
-    
-    filepath = '../../../download/separated_data/faces/'+path+'/box.txt'
-    with open(filepath) as fp:  
+ filepath = '../../../new_download/separated_data/faces/'+path+'/box.txt'
+ with open(filepath) as fp:  
+   line = fp.readline()
+   line = line.replace('[','')
+   line = line.replace(']','')
+   line = line.replace('(','')
+   line = line.replace(')','')
+   line = line.replace(' ','')
+   boxes=[line.strip()] 
+   #cnt = 1
+   while line:
       line = fp.readline()
+      line = line.strip()
       line = line.replace('[','')
       line = line.replace(']','')
       line = line.replace('(','')
       line = line.replace(')','')
       line = line.replace(' ','')
-      boxes=[line.strip()] 
-      #cnt = 1
-      while line:
-         line = fp.readline()
-         line = line.strip()
-         line = line.replace('[','')
-         line = line.replace(']','')
-         line = line.replace('(','')
-         line = line.replace(')','')
-         line = line.replace(' ','')
-         boxes.append(line)
-    boxes.pop()
-    for i in range(75):
-        boxes[i] = boxes[i].split(',')
+      boxes.append(line)
+ boxes.pop()
+ for i in range(75):
+    boxes[i] = boxes[i].split(',')
     #print(boxes[i])
  #print(type(boxes[30][0]))
  #boxes = open('output.txt', 'r')
  #print(boxes[30])
  #print(int(boxes[30][0]),int(boxes[30][1]), int(boxes[30][2]), int(boxes[30][5]))
 
- if __name__ == '__main__':
-   for i in range(75):
-      image = '../../../download/separated_data/faces/'+path+'/out'+str(i)+'.jpg'
-      crop(image, (int(boxes[i][0]),int(boxes[i][1]), int(boxes[i][2]), int(boxes[i][5])), '../../../download/separated_data/faces1/'+path+'/cropped'+str(i)+'.jpg')
+ 
+ for i in range(75):
+    image = '../../../new_download/separated_data/faces/'+path+'/out'+str(i)+'.jpg'
+    try:
+        os.makedirs('../../../new_download/separated_data/faces1/'+path)
+    except FileExistsError:
+        pass
+    crop(image, (int(boxes[i][0]),int(boxes[i][1]), int(boxes[i][2]), int(boxes[i][5])), '../../../download/separated_data/faces1/'+path+'/cropped'+str(i)+'.jpg')
 
