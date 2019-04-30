c12c253cc534d6860bf988a8001de2a438f40e81
Fan1117 <macfan1995@gmail.com>
2019-04-30 11:15:36 -0400

    update for frame

--- src/extract_face/cropface.py
+++ src/extract_face/cropface.py
@@ -14,33 +14,48 @@ def crop(image_path, coords, saved_location):
     cropped_image.save(saved_location)
     cropped_image.show()
 
-paths = ['ymD5uLlLc0g_036.033000-040.900000','Swss72CHSWg_090.023267-097.297200',
-        'Swss72CHSWg_090.023267-097.297200','AvWWVOgaMlk_090.000000-093.566667',
-         'akwvpAiLFk0_144.680000-150.000000']
+#paths = ['ymD5uLlLc0g_036.033000-040.900000','Swss72CHSWg_090.023267-097.297200',
+#        'Swss72CHSWg_090.023267-097.297200','AvWWVOgaMlk_090.000000-093.566667',
+#         'akwvpAiLFk0_144.680000-150.000000']
+
+import os
+frame_folder = '../../../download/separated_data/frame'
+
+paths = []
+for (dirpath, dirnames, filenames) in os.walk(frame_folder):
+    paths.extend(dirnames)
+    break
+
+
 
 for path in paths:
- filepath = '../../../download/separated_data/faces/'+path+'/box.txt'
- with open(filepath) as fp:  
-   line = fp.readline()
-   line = line.replace('[','')
-   line = line.replace(']','')
-   line = line.replace('(','')
-   line = line.replace(')','')
-   line = line.replace(' ','')
-   boxes=[line.strip()] 
-   #cnt = 1
-   while line:
+    try:
+        os.makedirs('../../../download/separated_data/faces/'+path)
+    except FileExistsError:
+        pass
+    
+    filepath = '../../../download/separated_data/faces/'+path+'/box.txt'
+    with open(filepath) as fp:  
       line = fp.readline()
-      line = line.strip()
       line = line.replace('[','')
       line = line.replace(']','')
       line = line.replace('(','')
       line = line.replace(')','')
       line = line.replace(' ','')
-      boxes.append(line)
- boxes.pop()
- for i in range(75):
-    boxes[i] = boxes[i].split(',')
+      boxes=[line.strip()] 
+      #cnt = 1
+      while line:
+         line = fp.readline()
+         line = line.strip()
+         line = line.replace('[','')
+         line = line.replace(']','')
+         line = line.replace('(','')
+         line = line.replace(')','')
+         line = line.replace(' ','')
+         boxes.append(line)
+    boxes.pop()
+    for i in range(75):
+        boxes[i] = boxes[i].split(',')
     #print(boxes[i])
  #print(type(boxes[30][0]))
  #boxes = open('output.txt', 'r')
