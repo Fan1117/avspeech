f5961a97503d95f9f0f1e09e99d1a404a8d42d8d
Fan1117 <macfan1995@gmail.com>
2019-05-01 20:29:42 -0400

    update for frame

--- src/con_train_model.py
+++ src/con_train_model.py
@@ -18,7 +18,7 @@ val_dataset = '../../new_dataset/audio_video/val_set.hdf5'
 test_dataset = '../../new_dataset/audio_video/test_set.hdf5'
 
 batch_size = 10
-epochs = 5
+epochs = 1
 
 train_generator = data_generator(train_dataset, batch_size)
 val_generator = data_generator(val_dataset, batch_size)
@@ -167,7 +167,7 @@ val_generator = data_generator(val_dataset, batch_size)
 #print("spec1's shape", spec1.shape)
 #spec2 = Lambda(lambda x : tf.multiply(x[0], x[1]), name = "multiply2")([A1, mask2])
 #print("spec2's shape", spec2.shape)
-model_path = '../../model/AV_30.h5'
+model_path = '../../model/AV_30_con1.h5'
 AV = keras.models.load_model(model_path, custom_objects={'tf':tf})
 # es = EarlyStopping(monitor='loss', min_delta=0, patience= 30,
 #                    verbose=1, mode='min', baseline=None, restore_best_weights= True)
@@ -175,10 +175,15 @@ ES = EarlyStopping(monitor='loss',patience=5)
 # Reduce learning rate when a metric has stopped improving.
 # rp = ReduceLROnPlateau(monitor=['loss'], factor=0.01, patience=5, verbose=1, mode='auto',
 #                        min_delta=0.0001, cooldown=0, min_lr=0)
+#tb = TensorBoard(log_dir='./tb_logs', histogram_freq=0, batch_size= batch_size,
+#                 write_graph=True, write_grads=False, write_images=False,
+#                 embeddings_freq=0, embeddings_layer_names=None,
+#                 embeddings_metadata=None, embeddings_data=None, update_freq='epoch')
 tb = TensorBoard(log_dir='./tb_logs', histogram_freq=0, batch_size= batch_size,
-                 write_graph=True, write_grads=False, write_images=False,
+                 write_graph=True, write_grads=False, write_images=True,
                  embeddings_freq=0, embeddings_layer_names=None,
                  embeddings_metadata=None, embeddings_data=None, update_freq='epoch')
+
 AV.compile(loss='mean_squared_error', optimizer='Adam',metrics=['accuracy'])
 
 #history = AV.fit([spec_mix,video_1,video_2],[spec_1,spec_2],
@@ -202,7 +207,7 @@ try:
     os.makedirs(model_dir)
 except FileExistsError:
     pass
-AV.save(model_dir + '/AV_30_con1.h5')
+AV.save(model_dir + '/AV_30_con2.h5')
 
 
 import json
@@ -213,11 +218,11 @@ except FileExistsError:
     pass
 
 # save json
-with open(history_dir + '/history_30_con1.json', 'w') as fp:
+with open(history_dir + '/history_30_con2.json', 'w') as fp:
     r = json.dump(history.history, fp, indent=2)
 
 # read json
-with open(history_dir + '/history_30_con1.json', 'r') as fp:
+with open(history_dir + '/history_30_con2.json', 'r') as fp:
     history = json.load(fp)
     print(history)
 
