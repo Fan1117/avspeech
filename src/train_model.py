3cba055b1ffac86d8231e91af33c7a708f9259e0
Fan1117 <macfan1995@gmail.com>
2019-05-05 15:14:21 -0400

    update for frame

--- src/train_model.py
+++ src/train_model.py
@@ -17,7 +17,7 @@ train_dataset = '../../100_dataset/audio_video/tr_set.hdf5'
 val_dataset = '../../100_dataset/audio_video/val_set.hdf5'
 test_dataset = '../../100_dataset/audio_video/test_set.hdf5'
 
-batch_size = 100
+batch_size = 20
 epochs = 100
 
 train_generator = data_generator(train_dataset, batch_size)
